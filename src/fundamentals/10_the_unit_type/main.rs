a0aebd3139e0c178958f92530fb6751baacb3dc0
KIMURA Takahiro <nanjakkun@gmail.com>
2025-12-13 05:50:17 +0900

    Solve track 10

---
+++ src/fundamentals/10_the_unit_type/main.rs
@@ -0,0 +1,7 @@
+/*
+You need to implement the function print_message() -> () that prints the message "Hello, Rust!" to the console.
+*/
+pub fn print_message() -> () {
+    // TODO: Implement the function here
+    println!("Hello, Rust!");
+}
