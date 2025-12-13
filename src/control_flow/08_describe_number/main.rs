3888f8282467445e7f1827bf1e978a8b637476b9
KIMURA Takahiro <nanjakkun@gmail.com>
2025-12-14 07:36:02 +0900

    Solve control flow track 9

--- src/control_flow/08_describe_number/main.rs
+++ src/control_flow/08_describe_number/main.rs
@@ -1,24 +1,4 @@
-/*
-    If the number is positive and even, return "Positive even".
-    If the number is positive and odd, return "Positive odd".
-    If the number is negative and even, return "Negative even".
-    If the number is negative and odd, return "Negative odd".
-    If the number is zero, return "Zero".
-*/
-pub fn describe_number(n: i32) -> String {
-    if n == 0 {
-        "Zero".to_string()
-    } else if n > 0 {
-        if n % 2 == 0 {
-            "Positive even".to_string()
-        } else {
-            "Positive odd".to_string()
-        }
-    } else {
-        if n % 2 == 0 {
-            "Negative even".to_string()
-        } else {
-            "Negative odd".to_string()
-        }
-    }
+pub fn factorial(n: u32) -> u128 {
+    // Implement your code here
+    (1..=n).fold(1, |acc, x| acc * x as u128)
 }
