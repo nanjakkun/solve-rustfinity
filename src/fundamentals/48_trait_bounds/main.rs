4c4987a994000e303049af4c94d5506152b2aa86
KIMURA Takahiro <nanjakkun@gmail.com>
2025-12-13 22:53:13 +0900

    Solve track 11

--- src/fundamentals/48_trait_bounds/main.rs
+++ src/fundamentals/48_trait_bounds/main.rs
@@ -7,14 +7,19 @@
     Use trait bounds to ensure the function works only with types that implement both Display and PartialOrd.
 */
 
+use std::cmp::PartialOrd;
+use std::fmt::Display;
+
 // TODO: Define the generic function `compare_and_display` with appropriate trait bounds.
-pub fn compare_and_display // Complete the function definition
+pub fn compare_and_display<T: Display + PartialOrd>(a: T, b: T) -> T {
+    if a > b { a } else { b }
+}
 
 // Example usage
 pub fn main() {
-     let greater = compare_and_display(10, 20);
-     println!("Greater value: {}", greater);
+    let greater = compare_and_display(10, 20);
+    println!("Greater value: {}", greater);
 
-     let greater = compare_and_display("Apple", "Orange");
-     println!("Greater value: {}", greater);
+    let greater = compare_and_display("Apple", "Orange");
+    println!("Greater value: {}", greater);
 }
